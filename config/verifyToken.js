8b34e8d93bf2eb6b463951796d1717ed20ae3449
rezarestian <114054889+restianreza@users.noreply.github.com>
2022-12-13 16:26:37 +0700

    uo

--- config/verifyToken.js
+++ config/verifyToken.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken')
-const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization')
-    if (!token) return res.status(400).json({
-        status: res.statusCode,
-        message: 'Access denied'
-    })
+// const jwt = require('jsonwebtoken')
+// const verifyToken = (req, res, next) => {
+//     const token = req.header('Authorization')
+//     if (!token) return res.status(400).json({
+//         status: res.statusCode,
+//         message: 'Access denied'
+//     })
 
-    try {
-        const verified = jwt.verify(token, process.env.SECRET_KEY) // verify
-        res.user = verified
-        next() // melanjutkan proses berikutnya asynch pada router
-    } catch (error) {
-        //jika token invalid,
-        //beri response 400 Invalid token
-        res.status(400).json({
-            status: res.statusCode,
-            message: 'Invalid token'
-        })
-    }
-}
+//     try {
+//         const verified = jwt.verify(token, process.env.SECRET_KEY) // verify
+//         res.user = verified
+//         next() // melanjutkan proses berikutnya asynch pada router
+//     } catch (error) {
+//         //jika token invalid,
+//         //beri response 400 Invalid token
+//         res.status(400).json({
+//             status: res.statusCode,
+//             message: 'Invalid token'
+//         })
+//     }
+// }
 
-module.exports = verifyToken
+// module.exports = verifyToken
